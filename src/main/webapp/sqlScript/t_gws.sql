162a67f516f8592128bc97d32c7682d4df5818b4
4gnosed <gnosed@foxmail.com>
2019-12-28 16:14:56 +0800

    数据

--- src/main/webapp/sqlScript/t_gws.sql
+++ src/main/webapp/sqlScript/t_gws.sql
@@ -1,70 +1,34 @@
-/*
- Navicat MySQL Data Transfer
-
- Source Server         : localhost_3306
- Source Server Type    : MySQL
- Source Server Version : 50725
- Source Host           : localhost:3306
- Source Schema         : zti
-
- Target Server Type    : MySQL
- Target Server Version : 50725
- File Encoding         : 65001
-
- Date: 13/12/2019 21:34:06
-*/
-
-SET NAMES utf8mb4;
-SET FOREIGN_KEY_CHECKS = 0;
-
--- ----------------------------
--- Table structure for t_gws
--- ----------------------------
-DROP TABLE IF EXISTS `t_gws`;
-CREATE TABLE `t_gws`  (
-  `gws_id` int(11) NOT NULL AUTO_INCREMENT,
-  `good_url_id` varchar(15) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `shop_url_id` varchar(15) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  PRIMARY KEY (`gws_id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 36 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Records of t_gws
--- ----------------------------
-INSERT INTO `t_gws` VALUES (1, '368591', '108710');
-INSERT INTO `t_gws` VALUES (2, '368592', '108710');
-INSERT INTO `t_gws` VALUES (3, '368591', '108710');
-INSERT INTO `t_gws` VALUES (4, '368591', '137391');
-INSERT INTO `t_gws` VALUES (5, '368592', '108710');
-INSERT INTO `t_gws` VALUES (6, '368592', '137391');
-INSERT INTO `t_gws` VALUES (7, '368593', '1356393');
-INSERT INTO `t_gws` VALUES (8, '368593', '137391');
-INSERT INTO `t_gws` VALUES (9, '368595', '1356393');
-INSERT INTO `t_gws` VALUES (10, '368595', '1356415');
-INSERT INTO `t_gws` VALUES (11, '368594', '1356393');
-INSERT INTO `t_gws` VALUES (12, '368594', '137391');
-INSERT INTO `t_gws` VALUES (13, '368596', '1356393');
-INSERT INTO `t_gws` VALUES (14, '368596', '137391');
-INSERT INTO `t_gws` VALUES (15, '368699', '1356638');
-INSERT INTO `t_gws` VALUES (16, '368699', '1356653');
-INSERT INTO `t_gws` VALUES (17, '368700', '1356638');
-INSERT INTO `t_gws` VALUES (18, '368704', '1356633');
-INSERT INTO `t_gws` VALUES (19, '368706', '1356650');
-INSERT INTO `t_gws` VALUES (20, '374244', '1356633');
-INSERT INTO `t_gws` VALUES (21, '374245', '1356638');
-INSERT INTO `t_gws` VALUES (22, '368618', '1356441');
-INSERT INTO `t_gws` VALUES (23, '368618', '1356452');
-INSERT INTO `t_gws` VALUES (24, '368621', '1356447');
-INSERT INTO `t_gws` VALUES (25, '368623', '1356441');
-INSERT INTO `t_gws` VALUES (26, '368596', '1356452');
-INSERT INTO `t_gws` VALUES (27, '368597', '137463');
-INSERT INTO `t_gws` VALUES (28, '368597', '108648');
-INSERT INTO `t_gws` VALUES (29, '368602', '137463');
-INSERT INTO `t_gws` VALUES (30, '368602', '108648');
-INSERT INTO `t_gws` VALUES (31, '368598', '137463');
-INSERT INTO `t_gws` VALUES (32, '368598', '108648');
-INSERT INTO `t_gws` VALUES (33, '368599', '137463');
-INSERT INTO `t_gws` VALUES (34, '368599', '108648');
-INSERT INTO `t_gws` VALUES (35, '368615', '22079');
-
-SET FOREIGN_KEY_CHECKS = 1;
+INSERT INTO `t_gws`(`gws_id`, `good_url_id`, `shop_url_id`) VALUES (1, '368591', '108710');
+INSERT INTO `t_gws`(`gws_id`, `good_url_id`, `shop_url_id`) VALUES (2, '368592', '108710');
+INSERT INTO `t_gws`(`gws_id`, `good_url_id`, `shop_url_id`) VALUES (3, '368591', '108710');
+INSERT INTO `t_gws`(`gws_id`, `good_url_id`, `shop_url_id`) VALUES (4, '368591', '137391');
+INSERT INTO `t_gws`(`gws_id`, `good_url_id`, `shop_url_id`) VALUES (5, '368592', '108710');
+INSERT INTO `t_gws`(`gws_id`, `good_url_id`, `shop_url_id`) VALUES (6, '368592', '137391');
+INSERT INTO `t_gws`(`gws_id`, `good_url_id`, `shop_url_id`) VALUES (7, '368593', '1356393');
+INSERT INTO `t_gws`(`gws_id`, `good_url_id`, `shop_url_id`) VALUES (8, '368593', '137391');
+INSERT INTO `t_gws`(`gws_id`, `good_url_id`, `shop_url_id`) VALUES (9, '368595', '1356393');
+INSERT INTO `t_gws`(`gws_id`, `good_url_id`, `shop_url_id`) VALUES (10, '368595', '1356415');
+INSERT INTO `t_gws`(`gws_id`, `good_url_id`, `shop_url_id`) VALUES (11, '368594', '1356393');
+INSERT INTO `t_gws`(`gws_id`, `good_url_id`, `shop_url_id`) VALUES (12, '368594', '137391');
+INSERT INTO `t_gws`(`gws_id`, `good_url_id`, `shop_url_id`) VALUES (13, '368596', '1356393');
+INSERT INTO `t_gws`(`gws_id`, `good_url_id`, `shop_url_id`) VALUES (14, '368596', '137391');
+INSERT INTO `t_gws`(`gws_id`, `good_url_id`, `shop_url_id`) VALUES (15, '368699', '1356638');
+INSERT INTO `t_gws`(`gws_id`, `good_url_id`, `shop_url_id`) VALUES (16, '368699', '1356653');
+INSERT INTO `t_gws`(`gws_id`, `good_url_id`, `shop_url_id`) VALUES (17, '368700', '1356638');
+INSERT INTO `t_gws`(`gws_id`, `good_url_id`, `shop_url_id`) VALUES (18, '368704', '1356633');
+INSERT INTO `t_gws`(`gws_id`, `good_url_id`, `shop_url_id`) VALUES (19, '368706', '1356650');
+INSERT INTO `t_gws`(`gws_id`, `good_url_id`, `shop_url_id`) VALUES (20, '374244', '1356633');
+INSERT INTO `t_gws`(`gws_id`, `good_url_id`, `shop_url_id`) VALUES (21, '374245', '1356638');
+INSERT INTO `t_gws`(`gws_id`, `good_url_id`, `shop_url_id`) VALUES (22, '368618', '1356441');
+INSERT INTO `t_gws`(`gws_id`, `good_url_id`, `shop_url_id`) VALUES (23, '368618', '1356452');
+INSERT INTO `t_gws`(`gws_id`, `good_url_id`, `shop_url_id`) VALUES (24, '368621', '1356447');
+INSERT INTO `t_gws`(`gws_id`, `good_url_id`, `shop_url_id`) VALUES (25, '368623', '1356441');
+INSERT INTO `t_gws`(`gws_id`, `good_url_id`, `shop_url_id`) VALUES (27, '368597', '137463');
+INSERT INTO `t_gws`(`gws_id`, `good_url_id`, `shop_url_id`) VALUES (28, '368597', '108648');
+INSERT INTO `t_gws`(`gws_id`, `good_url_id`, `shop_url_id`) VALUES (29, '368602', '137463');
+INSERT INTO `t_gws`(`gws_id`, `good_url_id`, `shop_url_id`) VALUES (30, '368602', '108648');
+INSERT INTO `t_gws`(`gws_id`, `good_url_id`, `shop_url_id`) VALUES (31, '368598', '137463');
+INSERT INTO `t_gws`(`gws_id`, `good_url_id`, `shop_url_id`) VALUES (32, '368598', '108648');
+INSERT INTO `t_gws`(`gws_id`, `good_url_id`, `shop_url_id`) VALUES (33, '368599', '137463');
+INSERT INTO `t_gws`(`gws_id`, `good_url_id`, `shop_url_id`) VALUES (34, '368599', '108648');
+INSERT INTO `t_gws`(`gws_id`, `good_url_id`, `shop_url_id`) VALUES (35, '368615', '22079');
--- src/main/webapp/sqlScript/t_shop.sql
+++ src/main/webapp/sqlScript/t_shop.sql
@@ -1,52 +1,14 @@
-/*
- Navicat MySQL Data Transfer
-
- Source Server         : localhost_3306
- Source Server Type    : MySQL
- Source Server Version : 50725
- Source Host           : localhost:3306
- Source Schema         : zti
-
- Target Server Type    : MySQL
- Target Server Version : 50725
- File Encoding         : 65001
-
- Date: 13/12/2019 21:33:45
-*/
-
-SET NAMES utf8mb4;
-SET FOREIGN_KEY_CHECKS = 0;
-
--- ----------------------------
--- Table structure for t_shop
--- ----------------------------
-DROP TABLE IF EXISTS `t_shop`;
-CREATE TABLE `t_shop`  (
-  `shop_id` int(11) NOT NULL AUTO_INCREMENT,
-  `shop_url_id` varchar(15) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `shop_name` varchar(30) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `shop_score` varchar(5) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
-  `comment_num` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
-  `shop_link` varchar(60) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  PRIMARY KEY (`shop_id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 15 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Records of t_shop
--- ----------------------------
-INSERT INTO `t_shop` VALUES (1, '108710', '微笑堂商厦', '4.3', ' (84条点评) ', '/shopping/guilin28/108710.html');
-INSERT INTO `t_shop` VALUES (2, '137391', '桂林八桂大厦', '4.2', ' (31条点评) ', '/shopping/guilin28/137391.html');
-INSERT INTO `t_shop` VALUES (3, '1356393', '万客隆购物广场', '4.3', ' (28条点评) ', '/shopping/guilin28/1356393.html');
-INSERT INTO `t_shop` VALUES (4, '1356415', '金顺昌特产店', '4.1', ' (27条点评) ', '/shopping/guilin28/1356415.html');
-INSERT INTO `t_shop` VALUES (5, '1356638', '南珠宫(北部湾中路店)', '4.4', ' (34条点评) ', '/shopping/beihai140/1356638.html');
-INSERT INTO `t_shop` VALUES (6, '1356653', '月升珍珠', '4.3', ' (29条点评) ', '/shopping/beihai140/1356653.html');
-INSERT INTO `t_shop` VALUES (7, '1356633', '北海市水产市场', '4.3', ' (273条点评) ', '/shopping/beihai140/1356633.html');
-INSERT INTO `t_shop` VALUES (8, '1356650', '本地人越南特产', '4.3', ' (46条点评) ', '/shopping/beihai140/1356650.html');
-INSERT INTO `t_shop` VALUES (9, '1356441', '南宁百货大楼', '4.4', ' (166条点评) ', '/shopping/nanning166/1356441.html');
-INSERT INTO `t_shop` VALUES (10, '1356452', '西关路夜市', '4.3', ' (135条点评) ', '/shopping/nanning166/1356452.html');
-INSERT INTO `t_shop` VALUES (11, '1356447', '朝阳路', '4.3', ' (159条点评) ', '/shopping/nanning166/1356447.html');
-INSERT INTO `t_shop` VALUES (12, '137463', '万客隆购物广场', '4.3', ' (24条点评) ', '/shopping/yangshuo702/137463.html');
-INSERT INTO `t_shop` VALUES (13, '108648', '99自选商场(叠翠路)', '4.2', ' (31条点评) ', '/shopping/yangshuo702/108648.html');
-INSERT INTO `t_shop` VALUES (14, '22079', '西街', '4.3', ' (999+条点评)', '/shopping/yangshuo702/22079.html');
-
-SET FOREIGN_KEY_CHECKS = 1;
+INSERT INTO `t_shop`(`shop_id`, `shop_url_id`, `shop_name`, `shop_score`, `comment_num`, `shop_link`) VALUES (1, '108710', '微笑堂商厦', '4.3', ' (84条点评) ', '/shopping/guilin28/108710.html');
+INSERT INTO `t_shop`(`shop_id`, `shop_url_id`, `shop_name`, `shop_score`, `comment_num`, `shop_link`) VALUES (2, '137391', '桂林八桂大厦', '4.2', ' (31条点评) ', '/shopping/guilin28/137391.html');
+INSERT INTO `t_shop`(`shop_id`, `shop_url_id`, `shop_name`, `shop_score`, `comment_num`, `shop_link`) VALUES (3, '1356393', '万客隆购物广场', '4.3', ' (28条点评) ', '/shopping/guilin28/1356393.html');
+INSERT INTO `t_shop`(`shop_id`, `shop_url_id`, `shop_name`, `shop_score`, `comment_num`, `shop_link`) VALUES (4, '1356415', '金顺昌特产店', '4.1', ' (27条点评) ', '/shopping/guilin28/1356415.html');
+INSERT INTO `t_shop`(`shop_id`, `shop_url_id`, `shop_name`, `shop_score`, `comment_num`, `shop_link`) VALUES (5, '1356638', '南珠宫(北部湾中路店)', '4.4', ' (34条点评) ', '/shopping/beihai140/1356638.html');
+INSERT INTO `t_shop`(`shop_id`, `shop_url_id`, `shop_name`, `shop_score`, `comment_num`, `shop_link`) VALUES (6, '1356653', '月升珍珠', '4.3', ' (29条点评) ', '/shopping/beihai140/1356653.html');
+INSERT INTO `t_shop`(`shop_id`, `shop_url_id`, `shop_name`, `shop_score`, `comment_num`, `shop_link`) VALUES (7, '1356633', '北海市水产市场', '4.3', ' (273条点评) ', '/shopping/beihai140/1356633.html');
+INSERT INTO `t_shop`(`shop_id`, `shop_url_id`, `shop_name`, `shop_score`, `comment_num`, `shop_link`) VALUES (8, '1356650', '本地人越南特产', '4.3', ' (46条点评) ', '/shopping/beihai140/1356650.html');
+INSERT INTO `t_shop`(`shop_id`, `shop_url_id`, `shop_name`, `shop_score`, `comment_num`, `shop_link`) VALUES (9, '1356441', '南宁百货大楼', '4.4', ' (166条点评) ', '/shopping/nanning166/1356441.html');
+INSERT INTO `t_shop`(`shop_id`, `shop_url_id`, `shop_name`, `shop_score`, `comment_num`, `shop_link`) VALUES (10, '1356452', '西关路夜市', '4.3', ' (135条点评) ', '/shopping/nanning166/1356452.html');
+INSERT INTO `t_shop`(`shop_id`, `shop_url_id`, `shop_name`, `shop_score`, `comment_num`, `shop_link`) VALUES (11, '1356447', '朝阳路', '4.3', ' (159条点评) ', '/shopping/nanning166/1356447.html');
+INSERT INTO `t_shop`(`shop_id`, `shop_url_id`, `shop_name`, `shop_score`, `comment_num`, `shop_link`) VALUES (12, '137463', '万客隆购物广场', '4.3', ' (24条点评) ', '/shopping/yangshuo702/137463.html');
+INSERT INTO `t_shop`(`shop_id`, `shop_url_id`, `shop_name`, `shop_score`, `comment_num`, `shop_link`) VALUES (13, '108648', '99自选商场(叠翠路)', '4.2', ' (31条点评) ', '/shopping/yangshuo702/108648.html');
+INSERT INTO `t_shop`(`shop_id`, `shop_url_id`, `shop_name`, `shop_score`, `comment_num`, `shop_link`) VALUES (14, '22079', '西街', '4.3', ' (999+条点评)', '/shopping/yangshuo702/22079.html');
